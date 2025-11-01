dd12f6ba3bc6a63551ca7fa16350ae63d9b3a31d
aintnoBaka7227 <phongtrando2004@gmail.com>
2025-11-01 17:34:13 +1030

    fix: exit on all system failures including threads and argument creation

--- comp2002-os-mergesort/mergesort.d
+++
@@ -1,2 +0,0 @@
-mergesort.o: mergesort.c mergesort.h
-mergesort.h:
--- comp2002-os-mergesort/mergesort_phong.c
+++ comp2002-os-mergesort/mergesort_phong.c
@@ -11,7 +11,6 @@
 void merge(int leftstart, int leftend, int rightstart, int rightend){
 	int i = leftstart, j = rightstart, k = leftstart;
 
-	// add each number from sorted subarrays to array b
 	while (i <= leftend && j <= rightend) {
 		if (A[i] <= A[j]) {
 			B[k] = A[i];
@@ -23,84 +22,88 @@ void merge(int leftstart, int leftend, int rightstart, int rightend){
 		k++;
 	}
 
-	// add the leftover numbers from one of the subarrays
-	// one subarrays will be fully added while the other still have leftover numbers
 	while (i <= leftend) B[k++] = A[i++];
 
 	while (j <= rightend) B[k++] = A[j++];
 
-	// copy sorted array b to a
 	memcpy(&A[leftstart], &B[leftstart], (rightend - leftstart + 1) * sizeof(int));
 }
 
 /* this function will be called by parallel_mergesort() as its base case. */
 void my_mergesort(int left, int right){
-	// no sort if left and right index cross
 	if (left < right) {
 		int mid = left + (right - left) / 2;
 
-		// merge sort left and right subarrays
 		my_mergesort(left, mid);
 		my_mergesort(mid + 1, right);
-		// merge both sorted subarrays
+
 		merge(left, mid, mid + 1, right);
 	}
 }
 
 /* this function will be called by the testing program. */
 void * parallel_mergesort(void *arg){
-
 	struct argument *a = (struct argument*) arg;
 	int left = a->left;
 	int right = a->right;
 	int level = a->level;
 
-	// no sort if left and right index cross
 	if (left >= right) {
 		if (level > 0) free(arg);
 		return NULL; 
 	}
 
-	// only sparse threads if not meet cut off level yet
 	if (level >= cutoff) {
 		my_mergesort(left, right);
 		if (level > 0) free(arg);
 		return NULL;
 	}
 
-	// split subarrays for multithreading
 	int mid = left + (right - left) / 2;
-	// create arguments for both left and right threads
 	struct argument *left_arg = buildArgs(left, mid, level + 1);
 	struct argument *right_arg = buildArgs(mid + 1, right, level + 1);
 
+	if (left_arg == NULL || right_arg == NULL) {
+		free(left_arg);
+		free(right_arg);
+		my_mergesort(left, right);
+		if (level > 0) free(arg);
+		return NULL;
+	}
+
 	pthread_t thread_left, thread_right;
-	// return flags for thread create
 	int left_flag = 0, right_flag = 0;
 	left_flag = pthread_create(&thread_left, NULL, parallel_mergesort, left_arg);
 	// handle left thread failure
-	// exit if fail to create
 	if (left_flag != 0) {
+		perror("Fail to create left thread");
 		fprintf(stderr, "Error: fail to create left thread with left %d, right %d, level %d!\n", left, mid, level+1);
-		// free unused arguments
+		// free both argument
 		free(left_arg);
 		free(right_arg);
+		my_mergesort(left, right);
+
 		if (level > 0) free(arg);
-		exit(EXIT_FAILURE);
+		return NULL;
 	}
 
 	right_flag = pthread_create(&thread_right, NULL, parallel_mergesort, right_arg);
 	// handle right thread failure
-	// exit if fail to create
 	if (right_flag != 0) {
+		perror("Fail to create right thread");
 		fprintf(stderr, "Error: fail to create right thread with left %d, right %d, level %d!\n", mid+1, right, level+1);
-		// wait for left thread to join 
-		pthread_join(thread_left, NULL);
-		// free unused right argument
+		// free idle right argument
 		free(right_arg);
-		// free this thread argument 
+
+		// wait for left thread to join after execution
+		pthread_join(thread_left, NULL);
+		// fallback to normal sort
+		my_mergesort(mid+1, right);
+		// merge result from left thread and normal sort right thread
+		merge(left, mid, mid+1, right);
+		// only free if not level 0 argument
 		if (level > 0) free(arg);
-		exit(EXIT_FAILURE);
+		return NULL;
 	}
 
 	// safely join threads after execution
@@ -110,7 +113,6 @@ void * parallel_mergesort(void *arg){
 	merge(left, mid, mid + 1, right);
 
 	// only free if not level 0 argument
-	// each thread free its own arg
 	if (level > 0) free(arg);
 	return NULL;
 }
@@ -119,11 +121,11 @@ void * parallel_mergesort(void *arg){
 struct argument * buildArgs(int left, int right, int level){
 	struct argument * arg = malloc(sizeof(struct argument));
 
-	// check malloc safely, mark fail if can not generate argument
+	/* check malloc safety, mark fail if can not generate arg */
 	if (arg == NULL) {
 		perror("Fail to allocate argument");
 		fprintf(stderr, "Error: malloc for arg with left %d, right %d, level %d failed!\n", left, right, level);
-		exit(EXIT_FAILURE);
+		return NULL;
 	}
 
 	arg->left = left;
